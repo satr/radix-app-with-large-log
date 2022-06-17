48e8c6f93a85253a2796ad2aba5d4024ee2cadcf
Sergey Smolnikov <ssmol@equinor.com>
2022-06-17 15:23:00 +0200

    Change

--- frontend/app/go.mod
+++ frontend/app/go.mod
@@ -3,8 +3,13 @@ module github.com/equinor/radix-app-with-large-log/frontend/app
 go 1.18
 
 require (
+	github.com/go-co-op/gocron v1.14.0
 	github.com/sirupsen/logrus v1.8.1
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
 )
 
-require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+require (
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- frontend/app/main.go
+++ frontend/app/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/go-co-op/gocron"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/utils/env"
 )
@@ -15,13 +17,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Radix app</h1>")
 	count, _ := env.GetInt("COUNT", 1200)
 	for i := 0; i < count; i++ {
-		totalCount += 1
-		log.Infof("Log line #%d ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ",
-			totalCount)
+		logLine()
 	}
 }
 
+func logLine() {
+	totalCount += 1
+	log.Infof("Log line #%d ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ ABCDEFGHIKLMNOPQRSTVXYZ",
+		totalCount)
+}
+
 func main() {
+	go func() {
+		done := make(chan bool)
+		waitScheduler := gocron.NewScheduler(time.UTC).SingletonMode()
+		waitScheduler.Every(5).Seconds().Do(func() {
+			logLine()
+		})
+		waitScheduler.StartAsync()
+		<-done
+		waitScheduler.Stop()
+	}()
 	fmt.Println("Start server")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8082", nil))
